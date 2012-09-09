eaf8de58e74a132f6a3fca416167322ddc39a45a
aix27249 <i27249@gmail.com>
2012-09-09 20:06:12 +0400

    fix version in kernelmod-build.sh

--- kernelmod-build.sh
+++ kernelmod-build.sh
@@ -3,14 +3,13 @@
 # Script used to help building package for multiple kernel branches
 
 PLAIN=3.4.7
-UKSM=3.4.7
+UKSM=3.4.7uksm
 LTS=3.0.42lts
 MED=3.5.3-med
 
 PKGNAME=$1
 
-( cd $PKGNAME ; KERNEL=$PLAIN mkpkg )
 ( cd $PKGNAME-uksm ; KERNEL=$UKSM mkpkg )
 ( cd $PKGNAME-lts ; KERNEL=$LTS mkpkg )
 ( cd $PKGNAME-med ; KERNEL=$MED mkpkg )
-
+( cd $PKGNAME ; KERNEL=$PLAIN mkpkg )
