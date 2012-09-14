c4d575530a1205e01d81f6ff735f5d333357b142
repository <admin@agilialinux.ru>
2012-09-14 22:34:44 +0400

    gvfs-gnome 1.8.2-3 x86_64 by aix27249

--- gvfs-gnome/files/doinst.sh
+++ gvfs-gnome/files/doinst.sh
@@ -1,5 +1,6 @@
-usr/bin/gio-querymodules usr/lib/gio/modules
-usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas
+chroot . usr/bin/gio-querymodules usr/lib/gio/modules
+# This stuff produces tons of warnings, we know about it, they cannot be solved with this version of gvfs, so let them go to /dev/null:
+chroot . usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas 2>/dev/null >/dev/null
 
 if [ "`pwd`" = "/" ] ; then
 	killall -USR1 gvfsd > /dev/null
