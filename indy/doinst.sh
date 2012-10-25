21111523969df211c973a7fc1284df3f6730a836
repository <admin@agilialinux.ru>
2012-10-25 23:00:57 +0400

    indy 10.2.0.3-2 x86_64 by khvalera

--- indy/doinst.sh
+++ indy/doinst.sh
@@ -1,6 +1,3 @@
-name_gr="lazarus"
-FPCVER=$(fpc -iV)
-
 if [ "$(uname -m)" == "x86_64" ]; then
    LIBDIRSUFFIX=64
 fi
@@ -16,10 +13,3 @@ else
 -Fu/usr/lib'${LIBDIRSUFFIX}'/lazarus/components/indy' etc/fpc.cfg
 fi
 echo "The configuration file fpc.cfg is changed"
-
-lazdir=/usr/lib${LIBDIRSUFFIX}/lazarus
-find ${lazdir} ! -group "${name_gr}" -print0 | xargs -0 --no-run-if-empty chgrp -v ${name_gr}
-find ${lazdir} ! -perm 02775 -type d -print0 | xargs -0 --no-run-if-empty chmod -v 02775
-find ${lazdir} ! -perm 0664 ! -perm 0755 ! -perm 0775 -type f -print0 | xargs -0 --no-run-if-empty chmod -v 0664
-find ${lazdir} -perm 0755 -type f -print0 | xargs -0 --no-run-if-empty chmod -v 0775
-find ${lazdir} ! -perm 0664 -name \*.bmp -type f -print0 -exec chmod -v 0664 {} \;
