3df8be5c1ccabd0cf2e7cca8400c015c5e55e6cb
khvalera <khvalera@narod.ru>
2012-10-29 14:08:43 +0300

    modified virtualbox/files/doinst.sh

--- virtualbox/files/doinst.sh
+++ virtualbox/files/doinst.sh
@@ -1,6 +1,8 @@
-GRP=`cat etc/group | grep vboxusers | head -n 1`
-if [ "$GRP" = "" ] ; then
-	chroot . groupadd vboxusers
+name_gr="vboxusers"
+
+if ! [ "$(less /etc/group | grep ${name_gr})" ] ; then
+   groupadd -f ${name_gr}
+   echo "Created group vboxusers"
 fi
 
 udevadm control --reload-rules
