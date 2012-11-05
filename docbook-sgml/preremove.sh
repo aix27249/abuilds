8ba2f5326769217082323812f58a12552ef29bee
aix27249 <i27249@gmail.com>
2012-11-05 01:39:23 +0000

    docbook-sgml

---
+++ docbook-sgml/preremove.sh
@@ -0,0 +1,12 @@
+# DTD 3.1
+chroot . install-catalog --remove /etc/sgml/sgml-docbook-dtd-3.1.cat \
+     /usr/share/sgml/docbook/sgml-dtd-3.1/catalog 
+chroot . install-catalog --remove /etc/sgml/sgml-docbook-dtd-3.1.cat \
+     /etc/sgml/sgml-docbook.cat
+
+# DTD 4.5
+chroot . install-catalog --remove /etc/sgml/sgml-docbook-dtd-4.5.cat \
+    /usr/share/sgml/docbook/sgml-dtd-4.5/catalog 
+chroot . install-catalog --remove /etc/sgml/sgml-docbook-dtd-4.5.cat \
+    /etc/sgml/sgml-docbook.cat
+
