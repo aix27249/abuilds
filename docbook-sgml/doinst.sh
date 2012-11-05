fc682a0068baaee5b28fab4331a4e04e1837f5d0
aix27249 <i27249@gmail.com>
2012-11-05 14:34:33 +0000

    docbook-sgml works!

--- docbook-sgml/doinst.sh
+++ docbook-sgml/doinst.sh
@@ -4,14 +4,6 @@ chroot . install-catalog --add /etc/sgml/sgml-docbook-dtd-3.1.cat \
 chroot . install-catalog --add /etc/sgml/sgml-docbook-dtd-3.1.cat \
      /etc/sgml/sgml-docbook.cat
 
-# Use only the most current 3.x version of DocBook SGML DTD:
-cat >> usr/share/sgml/docbook/sgml-dtd-3.1/catalog << "EOF"
-  -- Begin Single Major Version catalog changes --
-
-PUBLIC "-//Davenport//DTD DocBook V3.0//EN" "docbook.dtd"
-
-  -- End Single Major Version catalog changes --
-EOF
 
 # DTD 4.5
 chroot . install-catalog --add /etc/sgml/sgml-docbook-dtd-4.5.cat \
@@ -19,17 +11,4 @@ chroot . install-catalog --add /etc/sgml/sgml-docbook-dtd-4.5.cat \
 chroot . install-catalog --add /etc/sgml/sgml-docbook-dtd-4.5.cat \
     /etc/sgml/sgml-docbook.cat
 
-# Use only the most current 4.x version of DocBook SGML DTD :
-cat << EOF >> usr/share/sgml/docbook/sgml-dtd-4.5/catalog
-  -- Begin Single Major Version catalog changes --
-EOF
-for i in 4.4 4.3 4.2 4.1 4.0; do
-cat << EOF >> usr/share/sgml/docbook/sgml-dtd-4.5/catalog
-PUBLIC "-//OASIS//DTD DocBook V${i}//EN" "docbook.dtd"
-EOF
-done
-cat << EOF >> usr/share/sgml/docbook/sgml-dtd-4.5/catalog
-
-  -- End Single Major Version catalog changes --
-EOF
 
