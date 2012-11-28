d97736567f8b3f67f6001b1a1e619bf595df11aa
aix27249 <i27249@gmail.com>
2012-11-28 22:41:15 +0400

    kernel 3.6.8 bump

--- kernel/files/aufs_checkout.sh
+++ kernel/files/aufs_checkout.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-
+echo "Cloning AUFS, kernel branch: ${AUBRANCH}"
 git clone git://aufs.git.sourceforge.net/gitroot/aufs/aufs3-standalone.git \
 	aufs3-standalone
 cd aufs3-standalone
-git checkout origin/aufs3.5
+
+git checkout origin/aufs${AUBRANCH}
 
