b8d7478f8a665f12d2c5f02045c6af643cd0d16f
repository <admin@agilialinux.ru>
2012-12-09 16:52:11 +0400

    urbanterror 4.2.007-1 i686 by khvalera

--- urbanterror/files/urbanterror-server.sh
+++ urbanterror/files/urbanterror-server.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-#rename old folder if exists
-# [ ! -d ~/.urbanterror/baseut4 ] && [ -d ~/.urbanterror/q3ut4 ] && \
-# mv ~/.urbanterror/q3ut4 ~/.urbanterror/baseut4
+# cleanup mess, i did in last release
+if [ -d ~/.urbanterror/baseut4 ]; then
+	mkdir -p ~/.urbanterror/q3ut4
+	mv ~/.urbanterror/baseut4/*  ~/.urbanterror/q3ut4/
+	rmdir ~/.urbanterror/baseut4
+fi
 # start server
-cd /opt/urbanterror/
-exec ./ioUrTded "$@"
+exec ioUrTded +set fs_basepath "/usr/share/urbanterror"
+
