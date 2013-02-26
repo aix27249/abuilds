3cd5a35787c4017bb80bd63bc3b12104581c75bd
repository <admin@agilialinux.ru>
2013-02-26 12:56:29 +0400

    kernel 3.8.0-2 i686 by Ben

--- kernel/files/aufs_checkout.sh
+++ kernel/files/aufs_checkout.sh
@@ -4,5 +4,5 @@ git clone git://aufs.git.sourceforge.net/gitroot/aufs/aufs3-standalone.git \
 	aufs3-standalone
 cd aufs3-standalone
 
-git checkout origin/aufs${AUBRANCH}
+git checkout origin/aufs3.x-rcN
 
