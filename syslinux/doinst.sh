d9e249d76234754a81e938b2143df4418a1f7b86
repository <admin@agilialinux.ru>
2013-08-20 18:59:57 +0400

    syslinux 5.10-2 i686 by SirAnthony

--- syslinux/doinst.sh
+++ syslinux/doinst.sh
@@ -6,15 +6,13 @@ echo "==> to install it."
 # auto-update syslinux if /boot/syslinux/SYSLINUX_AUTOUPDATE exists
 /usr/bin/syslinux-install_update -s
 # update to 5.10 message
-if [ "$(vercmp $2 5.10)" -lt 0 ]; then
-  echo "If you used syslinux-install_update to install syslinux:"
-  echo "==> If you want to use syslinux with menu and all modules please rerun" 
-  echo "==>   # /usr/bin/syslinux-install_update -i -a -m"
-  echo ""
-  echo "If you manually installed syslinux:"
-  echo "==> Please copy or symlink all .c32 modules to your /boot/syslinux directory."
-  echo "==> If (/ and /boot on seperate fs):"
-  echo "==>   # cp /usr/lib/syslinux/*.c32 /boot/syslinux"
-  echo "==> If (/ and /boot on same fs):"
-  echo "==>   # ln -s /usr/lib/syslinux/*.c32 /boot/syslinux"
-fi
+echo "If you used syslinux-install_update to install syslinux:"
+echo "==> If you want to use syslinux with menu and all modules please rerun" 
+echo "==>   # /usr/bin/syslinux-install_update -i -a -m"
+echo ""
+echo "If you manually installed syslinux:"
+echo "==> Please copy or symlink all .c32 modules to your /boot/syslinux directory."
+echo "==> If (/ and /boot on seperate fs):"
+echo "==>   # cp /usr/lib/syslinux/*.c32 /boot/syslinux"
+echo "==> If (/ and /boot on same fs):"
+echo "==>   # ln -s /usr/lib/syslinux/*.c32 /boot/syslinux"
