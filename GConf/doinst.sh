ed554c5a3fc3dfff551dda808a2d5a54cd202af6
aix27249 <i27249@gmail.com>
2014-07-01 18:55:49 +0400

    GConf 2.32.0-3 noarch by khvalera

--- GConf/doinst.sh
+++
@@ -1,7 +0,0 @@
-ldconfig -r .
-chmod 755 etc/gconf/gconf.xml.system
-if [ "`uname -m`" = "x86_64" ] ; then
-	usr/bin/gio-querymodules usr/lib64/gio/modules
-else
-	usr/bin/gio-querymodules usr/lib/gio/modules
-fi
