29fd9d1f49c1f206a2de825adc53bf2ee87f4548
aix27249 <i27249@gmail.com>
2014-10-26 12:27:30 +0400

    tzdata 2014i-1 noarch by SirAnthony

--- tzdata/doinst.sh
+++
@@ -1,6 +0,0 @@
-echo "Update localtime"
-current_zone=`ls -l /etc/localtime-copied-from | awk -F "->" '{print $2}'`
-cp -f ${current_zone} /etc/localtime
-if [ -f /etc/init.d/ntp-client ]; then
-   /etc/init.d/ntp-client restart
-fi
