8565ce92aaeeb56df7a3efeb911c1b5b1dcb3d00
aix27249 <i27249@gmail.com>
2015-01-16 05:04:29 +0400

    kde 4.14.3-2 noarch by Ben

--- kde/doinst.sh
+++ kde/doinst.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-ln -s  /usr/lib/qt/bin/qtconfig /usr/bin/qtconfig
+ln -s  usr/lib/qt/bin/qtconfig usr/bin/qtconfig
+chmod 777 var/tmp
+echo "#"> etc/udev/rules.d/70-persistent-net.rules 
+echo "#"> lib/udev/rules.d/80-net-name-slot.rules
